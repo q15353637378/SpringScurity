f09c836a6e2bad89c149fc25c8fbfa70fdd86d48
[129qin] <[464815411@qq.com]>
2022-02-04 10:32:12 +0800

    master-final

--- src/main/java/com/qisicheng/config/SecurityConfig.java
+++ src/main/java/com/qisicheng/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    int i = 1;
     boolean loop = true;
 
     @Override
