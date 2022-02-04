59e46dbf7690afea93e33c34a37b70b7456b8381
[129qin] <[464815411@qq.com]>
2022-02-04 10:27:10 +0800

    hot-fix-second

--- src/main/java/com/qisicheng/config/SecurityConfig.java
+++ src/main/java/com/qisicheng/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    int i = 1;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
